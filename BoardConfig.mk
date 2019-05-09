2075b820597474fbc1a5b86a2bc3ebbe065f6b97
Arizaini Hidayat <famegalaxy614@gmail.com>
2019-05-10 05:48:43 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,3 +66,6 @@ TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_NTFS_3G := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_THEME := portrait_hdpi
+
+BOARD_USES_QCOM_HARDWARE := true (basic)
+TARGET_USES_QCOM_BSP := true (?)
